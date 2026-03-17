3eb842ec9032f1f7e90f7c344902d6b6cddae267
keedpek <zaharv20004@gmail.com>
2026-03-18 00:51:47 +0300

    init commit

---
+++ src/main/java/com/example/request_routing_service/RequestRoutingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.request_routing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestRoutingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestRoutingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/request_routing_service/RequestRoutingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.request_routing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestRoutingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
