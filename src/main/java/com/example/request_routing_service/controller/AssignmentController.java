d796dd2286aeebaf2b497752e4a79ee0a485942f
keedpek <zaharv20004@gmail.com>
2026-03-24 04:54:44 +0300

    feat: add validation

--- src/main/java/com/example/request_routing_service/controller/AssignmentController.java
+++ src/main/java/com/example/request_routing_service/controller/AssignmentController.java
@@ -1,7 +1,9 @@
 package com.example.request_routing_service.controller;
 
 import com.example.request_routing_service.service.AssignmentService;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -9,11 +11,19 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/routing/assign")
+@Validated
 public class AssignmentController {
   private final AssignmentService assignmentService;
 
   @PostMapping("/{requestId}")
-  public UUID assign(@PathVariable UUID requestId) {
-    return assignmentService.assign(requestId, "WEIGHTED");
+  public UUID assign(
+          @PathVariable UUID requestId,
+          @RequestParam(required = false, defaultValue = "WEIGHTED")
+          @Pattern(
+                  regexp = "^(WEIGHTED|SLA|WORKLOAD)$",
+                  message = "Стратегия: WEIGHTED, SLA или WORKLOAD")
+          String strategy
+  ) {
+    return assignmentService.assign(requestId, strategy);
   }
 }
